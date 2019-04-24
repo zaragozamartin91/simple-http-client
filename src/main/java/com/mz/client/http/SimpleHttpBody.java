7d345b277afb01a4785ad01507d62b653a795fc2
zaragozamartin91 <zaragozamartin91@gmail.com>
2019-04-23 21:45:25 -0300

    Se arreglaron metodos y se agrego javadoc

--- src/main/java/com/mz/client/http/SimpleHttpBody.java
+++ src/main/java/com/mz/client/http/SimpleHttpBody.java
@@ -3,25 +3,44 @@ package com.mz.client.http;
 public class SimpleHttpBody {
     private String body;
 
+    /**
+     * Creates a new Http body instance.
+     * 
+     * @param body Body content.
+     */
     public SimpleHttpBody(String body) {
         this.body = body;
     }
 
+    /**
+     * Checks if body has content.
+     * 
+     * @return True if body has content.
+     */
     public boolean isPresent() {
         return body != null;
     }
 
+    /**
+     * Gets body content.
+     * 
+     * @return body content, null if there's no content.
+     */
     public String value() {
         return body;
     }
 
+    /**
+     * Alias for {@link SimpleHttpBody#value()}.
+     * 
+     * @return body content, null if there's no content.
+     */
     public String get() {
-        return body;
+        return value();
     }
 
-    @Override public String toString() {
-        return "SimpleHttpBody{" +
-                "body='" + body + '\'' +
-                '}';
+    @Override
+    public String toString() {
+        return "SimpleHttpBody{" + "body='" + body + '\'' + '}';
     }
 }
