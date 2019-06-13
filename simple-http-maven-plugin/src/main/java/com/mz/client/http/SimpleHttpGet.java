03abbf3c426ed2b1bfd33691af6b24d610c2b5ee
zaragozamartin91-despegar <martin.zaragoza@despegar.com>
2019-06-13 16:26:15 -0300

    Se arreglan errores en el proyecto plugin

--- simple-http-maven-plugin/src/main/java/com/mz/client/http/SimpleHttpGet.java
+++ simple-http-maven-plugin/src/main/java/com/mz/client/http/SimpleHttpGet.java
@@ -1,14 +1,25 @@
+package com.mz.client.http;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
-/**
- * Says "Hi" to the user.
- *
- */
+
 @Mojo(name = "get")
 public class SimpleHttpGet extends AbstractMojo {
-    public void execute() throws MojoExecutionException {
-        getLog().info("Hello, world.");
+
+  @Parameter(property = "url", required = true)
+  private String url;
+
+  public void execute() throws MojoExecutionException {
+    SimpleHttpResponse response = SimpleHttpClient.newGet(url).execute();
+    SimpleHttpBody body = response.getBody();
+    if (body.isPresent()) {
+      getLog().info("BODY: " + body.get());
     }
+    int statusCode = response.getStatusCode();
+
+    getLog().info("Hello, world.");
+  }
 }
