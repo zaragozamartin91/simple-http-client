b7042b7a2037efb1aee0140c09e2324660a9d60d
zaragozamartin91-despegar <martin.zaragoza@despegar.com>
2019-06-13 11:58:46 -0300

    Se arreglan errores en la declaracion del plugin. Se agrega un proyecto plugin-tester

--- simple-http-maven-plugin/src/main/java/com/mz/client/http/SimpleHttpGet.java
+++ simple-http-maven-plugin/src/main/java/com/mz/client/http/SimpleHttpGet.java
@@ -6,7 +6,7 @@ import org.apache.maven.plugins.annotations.Mojo;
  * Says "Hi" to the user.
  *
  */
-@Mojo(name = "http-get")
+@Mojo(name = "get")
 public class SimpleHttpGet extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         getLog().info("Hello, world.");
